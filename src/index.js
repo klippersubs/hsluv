7cddb0783b8beeff3e676a0750ebde0d5a3f7fc2
aliceklipper <alice.klipper@yandex.com>
2017-09-20 02:39:49 +0300

    feat: Added alpha support, added HPLuv support, added aliases.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
-import { hsluvToHex } from 'hsluv';
+import { hsluvToHex, hsluvToRgb, hpluvToHex, hpluvToRgb } from 'hsluv';
 
-export default function hsluv(h, s, l) {
-    return hsluvToHex([h, s, l]);
-}
+export const hsluv = (h, s, l, a) => {
+    if (typeof a !== 'number') {
+        return hsluvToHex([h, s, l]);
+    } else {
+        return `rgba(${hsluvToRgb([h, s, l])
+            .map(x => Math.floor(x * 0xff))
+            .join(',')},${a})`;
+    }
+};
+
+export const hpluv = (h, s, l, a) => {
+    if (typeof a !== 'number') {
+        return hpluvToHex([h, s, l]);
+    } else {
+        return `rgba(${hpluvToRgb([h, s, l])
+            .map(x => Math.floor(x * 0xff))
+            .join(',')},${a})`;
+    }
+};
+
+export const hsl = hsluv;
+export const hpl = hpluv;
+
+export default hsluv;
